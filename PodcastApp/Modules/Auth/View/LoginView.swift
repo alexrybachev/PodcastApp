1c6a32ffd9be55296d9bfddfcf187d9e78824b07
Kirilloao <kirilltaraturin747@gmail>
2023-09-25 10:07:54 +0200

    create login and password properties

--- PodcastApp/Modules/Auth/View/LoginView.swift
+++ PodcastApp/Modules/Auth/View/LoginView.swift
@@ -8,10 +8,45 @@
 import UIKit
 
 final class LoginView: UIView {
-
+    
+    // MARK: - UI Properties
+    private lazy var loginStackView: UIStackView = {
+        var loginStackVIew = UIStackView()
+        loginStackVIew.backgroundColor = .green
+        return loginStackVIew
+    }()
+    
+    // MARK: - Login Properties
+    
+    lazy var loginLabel: UILabel = {
+        var loginLabel = UILabel()
+        return loginLabel
+    }()
+    
+    lazy var loginTextField: UITextField = {
+        var loginTF = UITextField()
+        loginTF.placeholder = "Login"
+        return loginTF
+    }()
+    
+    // MARK: - Password Properties
+    lazy var passwordLabel: UILabel = {
+        var passwordLabel = UILabel()
+        return passwordLabel
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        var passwordTF = UITextField()
+        passwordTF.placeholder = "Password"
+        return passwordTF
+    }()
+    
     // MARK: -  Init
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.backgroundColor = .white
+        self.addSubview(loginStackView)
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -19,4 +54,13 @@ final class LoginView: UIView {
     }
     
     // MARK: - Private Methods
+    private func setupConstraints() {
+        loginStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(140)
+            make.left.equalToSuperview().offset(16)
+            make.right.equalToSuperview().offset(-16)
+            make.height.equalTo(250)
+        }
+    }
+    
 }
