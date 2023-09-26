df9b35c847147bc7285cb433d60a0d3916f0eaff
Kirilloao <kirilltaraturin747@gmail.com>
2023-09-26 09:32:22 +0200

    create RegistrationViewController and RegistrationVIew

---
+++ PodcastApp/Modules/Auth/Controller/RegistrationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegistrationViewController.swift
+//  PodcastApp
+//
+//  Created by Kirill Taraturin on 26.09.2023.
+//
+
+import UIKit
+
+final class RegistrationViewController: UIViewController {
+    
+    // MARK: - Private UI Properties
+    
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupConstraints()
+    }
+    
+    // MARK: - Private methods
+    private func setupConstraints() {
+        
+    }
+    
+}
---
+++ PodcastApp/Modules/Auth/View/RegistrationView.swift
@@ -0,0 +1,32 @@
+//
+//  RegistrationView.swift
+//  PodcastApp
+//
+//  Created by Kirill Taraturin on 26.09.2023.
+//
+
+import UIKit
+
+final class RegistrationView: UIView {
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    
+    private func addViews() {
+        
+    }
+    
+    private func setupConstraints() {
+        
+    }
+}
