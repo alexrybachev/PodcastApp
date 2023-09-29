a7adbdfcc9a48dcf8618677c190d1e0ffbfa06d9
Elizaveta <eremeonock.el@gmail.com>
2023-09-29 11:43:08 +0300

    Add source with info about favorite Tracks

---
+++ PodcastApp/Modules/Favorites/File.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  PodcastApp
+//
+//  Created by Elizaveta Eremyonok on 29.09.2023.
+//
+
+import UIKit
+
+struct Track {
+    let title: String
+    let image: UIImage
+    let numberOfEpisodes: String
+}
+
+struct TrackSource {
+    static func makeTracks() -> [Track] {
+        [
+            .init(title: "Tuhan mengapa dia berbeda", image: UIImage(named: "ellipses")!, numberOfEpisodes: "15 Eps"),
+            .init(title: "Everyone want to be a superhero", image: UIImage(named: "ellipses")!, numberOfEpisodes: "55 Eps"),
+            .init(title: "Hobo man", image: UIImage(named: "ellipses")!, numberOfEpisodes: "54 Eps"),
+            .init(title: "Love and Friends", image: UIImage(named: "ellipses")!, numberOfEpisodes: "53 Eps"),
+            .init(title: "Friens but Married", image: UIImage(named: "ellipses")!, numberOfEpisodes: "15 Eps"),
+            .init(title: "Everyone want to be a superhero", image: UIImage(named: "ellipses")!, numberOfEpisodes: "55 Eps"),
+            .init(title: "Hobo man", image: UIImage(named: "ellipses")!, numberOfEpisodes: "54 Eps"),
+            .init(title: "Love and Friends", image: UIImage(named: "ellipses")!, numberOfEpisodes: "53 Eps")
+        ]
+    }
+}
