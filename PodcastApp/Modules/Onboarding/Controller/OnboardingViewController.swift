1051df098f9fb78ac27dcb4d6a7af012f6cfb87c
DmitryLorents <zmey.task@gmail.com>
2023-09-30 09:01:47 +0300

    Make onboarding navigation

--- PodcastApp/Modules/Onboarding/Controller/OnboardingViewController.swift
+++ PodcastApp/Modules/Onboarding/Controller/OnboardingViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
+final class OnboardingViewController: UIViewController {
+    
+   lazy var completedButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Onboarding finished!", for: .normal)
+        button.addTarget(nil, action: #selector(finishOnboarding), for: .touchUpInside)
+        button.backgroundColor = .systemGray
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemTeal
+        setViews()
+        setConstraints()
+    }
+    
+    //MARK: - Methods
+   private func setViews() {
+       view.backgroundColor = .systemTeal
+       view.addSubview(completedButton)
+    }
+    
+    private  func setConstraints() {
+        completedButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func finishOnboarding() {
+        AppSettingsManager.setCompleted()
+        let tabBarVC = CustomTabBarController()
+        tabBarVC.modalPresentationStyle = .fullScreen
+        present(tabBarVC, animated: true)
     }
 
 }
