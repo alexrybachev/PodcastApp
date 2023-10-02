d0e041650e13d4962c14743a013893ac0fb178a8
DmitryLorents <zmey.task@gmail.com>
2023-10-02 20:16:21 +0300

    Start playerView

---
+++ PodcastApp/Modules/Home/View/PlayerView.swift
@@ -0,0 +1,26 @@
+//
+//  PlayerView.swift
+//  PodcastApp
+//
+//  Created by Дмитрий Лоренц on 01.10.2023.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+    
+    private let backView: UIView = {
+        let view = UIView()
+        view.backgroundColor = 
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
