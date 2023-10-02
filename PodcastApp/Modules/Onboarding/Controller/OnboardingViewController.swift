28d2c906de4eb1a09bce5ba994e9210b6e3e166f
DmitryLorents <zmey.task@gmail.com>
2023-10-02 20:30:11 +0300

    Merge Kirill/Onboarding into develop manually

--- PodcastApp/Modules/Onboarding/Controller/OnboardingViewController.swift
+++ PodcastApp/Modules/Onboarding/Controller/OnboardingViewController.swift
@@ -9,38 +9,104 @@ import UIKit
 
 final class OnboardingViewController: UIViewController {
     
-   lazy var completedButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Onboarding finished!", for: .normal)
-        button.addTarget(nil, action: #selector(finishOnboarding), for: .touchUpInside)
-        button.backgroundColor = .systemGray
-        return button
-    }()
-
+    // MARK: - Privaet UI Properties
+    private let onboardingView = OnboardingView()
+    
+    // MARK: - Private Properties
+    private let infoSlides = Slide.getInfoSlides()
+    
+    // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemTeal
-        setViews()
-        setConstraints()
+        view.addSubview(onboardingView)
+        setupConstraints()
+        
+        onboardingView.transferDelegates(
+            dataSource: self,
+            delegate: self
+        )
+        
+        onboardingView.updateInfoLabels(infoSlides[0])
+        setupButtonAction()
+        setupNextButton()
+        onboardingView.setupPageControl(infoSlides.count)
     }
     
-    //MARK: - Methods
-   private func setViews() {
-       view.backgroundColor = .systemTeal
-       view.addSubview(completedButton)
-    }
-    
-    private  func setConstraints() {
-        completedButton.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+    // MARK: - Private Methods
+    private func setupConstraints() {
+        onboardingView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
     }
     
-    @objc func finishOnboarding() {
-        AppSettingsManager.setCompleted()
-        let tabBarVC = CustomTabBarController()
-        tabBarVC.modalPresentationStyle = .fullScreen
-        present(tabBarVC, animated: true)
+    // переход на таб бар после нажания на кнопку Start или skip
+    private func setupButtonAction() {
+        onboardingView.buttonAction = { [weak self] in
+            AppSettingsManager.setCompleted()
+            let tabBarVC = CustomTabBarController()
+            tabBarVC.modalPresentationStyle = .fullScreen
+            self?.present(tabBarVC, animated: true)
+        }
     }
+    
+    private func setupNextButton() {
+        onboardingView.nextButtonAction = { [weak self] in
+            self?.handleNextButtonTapped()
+        }
+    }
+    
+    // обновления UI при нажатии на кнопку nextButton
+    private func handleNextButtonTapped() {
+        if onboardingView.currentSlideIndex < onboardingView.pageControl.numberOfPages - 1 {
+            onboardingView.currentSlideIndex += 1
+            
+            let slide = infoSlides[onboardingView.currentSlideIndex]
+            onboardingView.updateInfoLabels(slide)
+            let indexPath = IndexPath(item: onboardingView.currentSlideIndex, section: 0)
+            onboardingView.mainCollectionView.scrollToItem(
+                at: indexPath,
+                at: .centeredHorizontally,
+                animated: true
+            )
+            onboardingView.pageControl.currentPage = onboardingView.currentSlideIndex
+            if onboardingView.currentSlideIndex == 2 {
+                onboardingView.skipButton.isHidden = true
+                onboardingView.nextButton.isHidden = true
+                onboardingView.startButton.isHidden = false
+            }
+        }
+    }
+}
 
+// MARK: - UICollectionViewDataSource
+extension OnboardingViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        infoSlides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "cell",
+                for: indexPath) as? ImageCell
+        else {
+            return UICollectionViewCell()
+        }
+        
+        let imageName =  infoSlides[indexPath.item].imageName
+        
+        cell.configure(with: imageName)
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension OnboardingViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        collectionView.frame.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
 }
